0eb33778fd957a7f5540757fb55f0bcc6c8ac1df
Derrick Watson <junkbeebox0@gmail.com>
2024-06-11 15:43:31 +0800

    Upload of new lab file

--- lab5.sh
+++
@@ -1,7 +0,0 @@
-gcloud app create --project $DEVSHELL_PROJECT_ID --region europe-west4
-gcloud app deploy
-gcloud app describe
-
-#Enable IAP and Configure OAuth 
-#User _gcp_iap/clear_login_cookie to clear cookies
-
